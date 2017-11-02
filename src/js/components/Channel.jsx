123431196f5581bf2d7f494eb2e1a89f54c6f600
Mrazator <marcellomraz@gmail.com>
2017-11-02 02:18:41 +0100

    Channel to functional component

--- src/js/components/Channel.jsx
+++ src/js/components/Channel.jsx
@@ -1,26 +1,24 @@
-import React, {Component} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import '../../styles/components/Channel.css'
 
-class Channel extends Component {
-    static propTypes = {
-        item: PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            customData: PropTypes.string
-        }).isRequired,
-        onDelete: PropTypes.func.isRequired,
-        onStartEditing: PropTypes.func.isRequired
-    }
-
-    render() {
-        return (
-            <li className="Channel">
-                <span onClick={() => this.props.onStartEditing(this.props.item.id)}>{this.props.item.name}</span>
-                <i className="icon-trash" onClick={() => this.props.onDelete(this.props.item.id)}/>
-            </li>
-        )
-    }
+function Channel(props) {
+    return (
+        <li className="Channel">
+            <span onClick={() => props.onStartEditing(props.item.id)}>{props.item.name}</span>
+            <i className="icon-trash" onClick={() => props.onDelete(props.item.id)}/>
+        </li>
+    )
 }
+
+Channel.propTypes = {
+    item: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        customData: PropTypes.string
+    }).isRequired,
+    onDelete: PropTypes.func.isRequired,
+    onStartEditing: PropTypes.func.isRequired
+};
 
 export default Channel
